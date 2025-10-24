0102799d362292f97cf12ec3cb3157ad9c524e90
alunoifpe <alunoifpe@CIGR-266224.ifpe.edu>
2025-10-24 15:56:34 -0300

    crud atividade 05 imcompleto

---
+++ atividades_laravel_web2/atividade_03/atividade03/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));  
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+        Category::create($request->all());
+
+        return redirect()->route('categories.index')->with('success', 'Categoria criada com sucesso.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        return view('categories.show', compact('category'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        return view('categories.show', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required|string|unique:categories,name' . $category->$id . '|max:255'
+        ]);
+
+        return redirect()->route('categories.index')->with('sucess', 'A CATEGORIA FOI ATUALIZADA COM SUCESSO.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('success', 'CATEGORIA DESTRUIDA COM SUCESSO');
+
+    }
+}
